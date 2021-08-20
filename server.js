cd850b00b8354cd959c60513f5c1ed1888f74690
Yoni <yonatansamfisher@gmail.com>
2021-08-20 15:54:08 +0300

    heroku deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const path = require('path')
+const express = require('express')
+const app = express();
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '/build')))
+
+    app.get('*',(req,res)=> {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    })
+}
